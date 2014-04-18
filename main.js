9eeefd7721389d57046f4cb921361658c58815b0
Diana Suvorova <dianasuvorova@Boriss-MacBook-Air.local>
2014-04-18 10:28:50 -0700

    different color categories

--- main.js
+++ main.js
@@ -15,20 +15,32 @@ var node_items=[];
 
 console.log(node_items);
 
-
+navigation_items=["NEURONS","YOGA","INFO","VISD3JS","BOOKMARKS BAR","OTHER BOOKMARKS","MOBILE BOOKMARKS"];
+node_items=[ {tags: "NEURONS",
+            title: "Figure 1 : Pyramidal neurons: dendritic structure and synaptic integration : Nature Reviews Neuroscience",
+            url: "http://www.nature.com/nrn/journal/v9/n3/fig_tab/nrn2286_F1.html"},
+            {tags: "NEURONS",
+              title: "Software Engineer at Chartio - Python - Stack Overflow Careers 2.0",
+              url: "http://careers.stackoverflow.com/jobs/48832/software-engineer-chartio?a=XyrH9zCU&searchTerm=%22data%20visualization%22"
+            },{
+              tags: "YOGA",
+              title: "Butterfly Yoga > Login Or Sign Up",
+              url: "https://www.secure-booker.com/butterflyyoga/LoginOrSignUp/LoginOrSignUp.aspx"}];
 
 
 $(document).ready(function(){  
 
-chrome.bookmarks.getTree(function(itemTree){
-    itemTree.forEach(function(item){
-        processNode(item,'');
-    });
+// chrome.bookmarks.getTree(function(itemTree){
+//     itemTree.forEach(function(item){
+//         processNode(item,'');
+//     });
 
-    renderUI();
-});
+//     renderUI();
+// });
 
-})
+// })
+
+renderUI(); })
 
 //Ithink it's ugly
   var local_tags='';
@@ -53,7 +65,7 @@ function processNode(node,tags) {
       navigation_items.push(node.title.toUpperCase()); }
 
     if(node.url) { 
-     console.log(node)
+    // console.log(node)
       node_items.push({
         url :node.url,
         title: node.title,
@@ -64,11 +76,8 @@ function processNode(node,tags) {
 
 }
 
-
-//console.log(bookmark_items);
-
-
-
+console.log(node_items);
+console.log(navigation_items);
 
 function renderUI(){
 
@@ -80,36 +89,27 @@ function renderUI(){
 
 var root = JSON.parse('{"id":"1","name":"Structure","children":[{"id":"2","name":"Media","children":[{"id":"3","name":"static","children":[{"id":"4","name":"text","size":"10"},{"id":"5","name":"picture","size":"10"}]},{"id":"6","name":"interaction","children":[{"id":"7","name":"D3","size":"10"}]},{"id":"8","name":"film","children":[{"id":"9","name":"animation","size":"10"},{"id":"10","name":"movie","size":"10"}]}]},{"id":"11","name":"Subject","children":[{"id":"12","name":"Neurosicence","children":[{"id":"13","name":"BIO","size":"10"},{"id":"14","name":"AI","size":"10"}]},{"id":"15","name":"Psychology","size":"10"},{"id":"16","name":"Philosophy","size":"10"},{"id":"17","name":"Art","size":"10"}]}]}');
 
-//console.log(images);
-// var media_categories=["picture","animation","video","text","D3"];
-// var subject_categories=["philosophy","art","psychology","Neurosicence"];
-
-
-// for (var i=0;i<media_categories.length;i++ ){
-//   media_categories[i]=media_categories[i].toUpperCase();
-// } 
-
-// for (var i=0;i<subject_categories.length;i++ ){
-//   subject_categories[i]=subject_categories[i].toUpperCase();
-// } 
-
-    var color = d3.scale.category20c();
+    var color = d3.scale.category10();
 
     //navigation part
+    var categories = d3.range(navigation_items.length).map(function(i){return {color:color(i),title:navigation_items[i]};});
+
 
 
     var bookmark = d3.select("#bookmark");
-    console.log(bookmark);
+    console.log('categories ' + categories);
 
-    var bookmark_width = Math.floor((bookmark.style("width").slice(0, -2)/navigation_items.length - 2*2)/bookmark.style("width").slice(0, -2)*100)+'%';
+    var bookmark_width = Math.floor((bookmark.style("width").slice(0, -2)/categories.length - 2*2)/bookmark.style("width").slice(0, -2)*100)+'%';
 
-    var bookmarks = bookmark.selectAll(".categories").data(navigation_items)
+    var bookmarks = bookmark.selectAll(".categories").data(categories)
                   .enter().append("div")
                   .attr("class","categories")
-                  .attr("id",function(d){return d.replace(/ /g,'')})
+                  .attr("id",function(d){return d.title.replace(/ /g,'')})
                   .style("width", bookmark_width)
+                  .on("mouseover",function(d){d3.select(this).style('color',d.color)})
+                  .on("mouseout",function(d){ if  (d3.select(this).attr("class")=='categories') d3.select(this).style('color','rgb(152,151,150)')})
                   .on("click", categories_clicked)
-                  .text(function(d){return d;});
+                  .text(function(d){return d.title;});
 
     ///Bubble collision part
 
@@ -120,7 +120,7 @@ var root = JSON.parse('{"id":"1","name":"Structure","children":[{"id":"2","name"
     // var padding=7.5;   
      var radius= 75; 
      var expanded_radius=240;
-     var padding=15;   
+     var padding=5;   
 
 
     //Get the size from HTML
@@ -128,8 +128,11 @@ var root = JSON.parse('{"id":"1","name":"Structure","children":[{"id":"2","name"
     var height= canvas.style("height").slice(0, -2);
 
 
-    var nodes = d3.range(node_items.length).map(function(i) { return { radius:radius+padding/2 , image : node_items[i] }; });
-    var  color = d3.scale.category10();
+    
+    //the detecting color index part coule be done on creating items step instead of traversing array over.
+
+    var nodes = d3.range(node_items.length).map(function(i) { return { radius:radius+padding/2 , image : node_items[i] , color : color(navigation_items.indexOf(node_items[i].tags)) }});
+
 
 
     var bubble = canvas.selectAll(".bubble").data(nodes)
@@ -163,35 +166,34 @@ var root = JSON.parse('{"id":"1","name":"Structure","children":[{"id":"2","name"
 
     function mouseover(d, i) {
 
-
        force.stop();
 
        d3.selectAll(".bubbleFill").transition().duration(250).ease("linear")
         .style("opacity", 0.5);
 
-        d3.select("#bubble-"+i).select('.bubbleFill').transition().duration(250).ease("linear")
+        d3.select(this).transition().duration(250).ease("linear")
         .style('width', expanded_radius+'px')
         .style('height', expanded_radius+'px')
-        .attr("class",function(){return $(this).attr("class")+" highlighted"})
+        .style("border-color",d.color)
         .style("z-index",1); 
 
-         d3.select("#bubble-"+i).append('div')
+         d3.select(this).append('div')
                   .attr('class','tooltip')
                   .transition().delay(250).duration(25).ease("linear")
                   .style("top",expanded_radius/4+3+"px")
                   .style("left",3+"px")
-                  .style("width",0)           
-                  .style("width","240px")
-                  .style("opacity",1)
+                  .style("background-color",d.color)         
+                  .style("opacity",0.7)
                   .style("z-index",2); 
 
-        d3.select("#bubble-"+i).select('div.tooltip')
+        d3.select(this).select('div.tooltip')
                   .append("span")
                   .transition().delay(250).duration(25).ease("linear")
-                  .text(function(d, i) { return ((d.image.url =="" ) ? "no link " : d.image.url )
-                                               + d.image.tags});
-         
+                  .text(function(d, i) { return ((d.image.url =="" ) ? "no link " : d.image.title )});
+    
+        //highlight category         
 
+        d3.selectAll("#"+d.image.tags).style('color',d.color);
     }
 
     function mouseout(d, i) {
@@ -202,11 +204,15 @@ var root = JSON.parse('{"id":"1","name":"Structure","children":[{"id":"2","name"
       .style("opacity", 1)
       .style('width', radius+'px')
       .style('height', radius+'px')
-      .attr("class",function(){return $(this).attr("class").replace(" highlighted", "")})
+      .style("border-color","rgb(179,179,179)")
    //   .style('border-color','rgb(179,179,179)')
       .style("z-index",0); 
 
        d3.selectAll("#bubble-"+i).select('.tooltip').remove();
+
+       d3.selectAll("#"+d.image.tags).style('color',function(){ return (d3.select(this).attr("class")=='categories')? 'rgb(152,151,150)': d.color ;});
+
+
 
       force.resume();
 
@@ -215,16 +221,21 @@ var root = JSON.parse('{"id":"1","name":"Structure","children":[{"id":"2","name"
 
     function categories_clicked(d,i){
 
-      if (d3.select("#"+d.replace(/ /g,'')).attr("class") == "categories"){
+      if (d3.select(this).attr("class") == "categories"){
         
-        d3.select("#"+d.replace(/ /g,'')).attr("class","categories selected");
-        bubble.selectAll("."+d.replace(/ /g,'')).attr("class",function(){return $(this).attr("class")+" highlighted"});
+        d3.select(this).attr("class","categories selected")
+        .style("color",d.color)
+        .style("border-color",d.color)
+        ;
+        bubble.selectAll("."+d.title.replace(/ /g,'')).style("border-color",d.color);
       }
 
       else{
 
-        d3.select("#"+d.replace(/ /g,'')).attr("class","categories");
-          bubble.selectAll("."+d.replace(/ /g,'')).attr("class",function(){return $(this).attr("class").replace(" highlighted", "")});
+        d3.select(this).attr("class","categories")
+        .style("color","rgb(152,151,150)")
+        .style("border-color","transparent");
+          bubble.selectAll("."+d.title.replace(/ /g,'')).style("border-color","rgb(179,179,179)");
 
         }
     }
