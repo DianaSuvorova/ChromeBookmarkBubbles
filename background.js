2bfa51d07c7348c533e144c68766ac359f5ee6f5
Diana Suvorova <dianasuvorova@Boriss-MacBook-Air.local>
2014-04-17 17:17:32 -0700

    1.1 on web store

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "index.html";
-  chrome.tabs.create({ url: newURL });
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "index.html";
+    chrome.tabs.create({ url: newURL });
 });
